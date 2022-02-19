68a9242afdeca1b52b43d57e074aeeaf31f99d9c
tmhglnd <timohoogland@gmail.com>
2022-02-19 16:07:52 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 700,
+		frame: false,
+		// titleBarStyle: 'hiddenInset'
+		// titleBarStyle: 'hidden',
+		icon: __dirname + '/public/assets/icon/mercury-logo.png'
+	});
+
+	win.loadFile('public/index.html');
+	win.setMenuBarVisibility(false);
+}
+
+app.setName('MERCURY PLAYGROUND');
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
